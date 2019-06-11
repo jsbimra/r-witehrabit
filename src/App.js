03112daeb65a77db00432616110bac687c6e8609
jsbimra <jsbimra@users.noreply.github.com>
2019-06-11 16:43:39 +0530

    react scaffold commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Peninsula Diagnostic Imaging</h1>
+        <h2>Request an appoitnment</h2>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
